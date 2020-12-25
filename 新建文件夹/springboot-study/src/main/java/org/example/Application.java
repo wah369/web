e1011b2ad4e5b7da1262ca8c2d3d844c912f8d49
wah369 <1808154326@qq.com>
2020-12-25 23:32:01 +0800

    EE浏览器动态资源

---
+++ 新建文件夹/springboot-study/src/main/java/org/example/Application.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 联想
+ * Date: 2020-12-22
+ * Time: 20:28
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
